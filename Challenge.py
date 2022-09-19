acc346fe568b39dd7eb4c6833a4f14733db943e3
OdoDuke <112731566+OdoDuke@users.noreply.github.com>
2022-09-19 14:01:03 +0100

    Add files via upload

---
+++ Challenge.py
@@ -0,0 +1,25 @@
+# A program to positively affirm people
+name = input("Welcome! What's your name? ")
+print ("Glad to have you here",name,"!")
+
+def pos_affi():
+    number = input("Please pick a number from 1-5 for a priceless gift: ")
+    if number == 1:
+        print ("Your laughter and smile comparet to none other, I hope you always have reasons to smile!")
+    elif number == 2:
+        print ("You are deserving of great opportunities and soon you will have an abundant flow of them!")
+    elif number == 3:
+        print ("You are loved for being who you are, just for existing")
+    elif number == 4:
+        print ('Ah ah! See as you set! Find a mirrror and tell the image you see "You are too much!"')
+    elif number == 5:
+        print ("Can you? Of course you can! You can do anything you put your mind to")
+
+pos_affi()
+pos_affi()
+pos_affi()
+pos_affi()
+pos_affi()
+
+print ("I hope you are much happier after this!")
+print ("See you next time",name,)
